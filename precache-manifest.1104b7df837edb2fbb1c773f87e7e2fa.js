6620d2f28cd8f0b62d350e0fc8a9d5b8ff1b55df
Tuukka Uskali <tuukka.uskali@meteoriitti.com>
2020-03-04 12:51:43 +0200

    Updates

---
+++ precache-manifest.1104b7df837edb2fbb1c773f87e7e2fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d343fd05bc76a12930af176d9e5c0ae",
+    "url": "/corona-dude/index.html"
+  },
+  {
+    "revision": "2303611a1590eee96ee2",
+    "url": "/corona-dude/static/css/main.9fa3b510.chunk.css"
+  },
+  {
+    "revision": "7080e8ed0d5995d7fcca",
+    "url": "/corona-dude/static/js/2.1b6b7b77.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/corona-dude/static/js/2.1b6b7b77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2303611a1590eee96ee2",
+    "url": "/corona-dude/static/js/main.57452ac4.chunk.js"
+  },
+  {
+    "revision": "aee6a942745300646071",
+    "url": "/corona-dude/static/js/runtime-main.c9c2c5ec.js"
+  },
+  {
+    "revision": "9f19440f74f1c8126fdcf003e3d024da",
+    "url": "/corona-dude/static/media/player_walk.9f19440f.png"
+  }
+]);
